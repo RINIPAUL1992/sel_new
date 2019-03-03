0018dfb3f4d655894bc4986f6eb8a69e88992d1f
rini paul <rinimukkathu@gmail.com>
2019-03-03 17:56:29 +0530

    multiple window code

---
+++ browsr_contrl.py
@@ -0,0 +1,11 @@
+from  selenium import webdriver
+import time
+
+driver=webdriver.Chrome()
+driver.get("https://in.bookmyshow.com/bengaluru")
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.find_element_by_xpath("//a[text()='Movies']").click()
+driver.find_element_by_xpath("//a[text()='Events']").click()
+driver.back()
+driver.forward()
